f86a1c6c9e47dd28591014e900bda38a771423dd
Herve176 <hdjomo1@gmail.com>
2023-12-31 00:22:57 +0100

    .

--- first_flutter/lib/main.dart
+++ first_flutter/lib/main.dart
@@ -1,5 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Beautiful Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Beautiful App Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to Your Flutter App!',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            FlutterLogo(
+              size: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
